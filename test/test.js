b374687eb9b019c06258f93f2aca8e358151492a
tallgy <1776232657@qq.com>
2023-09-09 00:08:31 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 let express = require('..')
 let app = express()
 
-// app.get('/',(req,res) => res.end('hello world'))
-
-app.use('/user1/:idq', (req, res, next) => {
+app.use('/user/:id1', (req, res, next) => {
   console.log(3);
   next();
 })
@@ -13,25 +11,10 @@ app.get('/user1/:id', (req, res, next) => {
   next()
 })
 
-// app.use(express.static('./public'))
-
-
-// app.param(['id', 'page'], function (req, res, next, value) {
-//   console.log('param', value)
-//   next()
-// })
-
-
-// app.param((name, fn) => {
-//   console.log('name fn', name, fn);
-//   return function (req, res, next, value) {
-//     console.log('param -- 1', value)
-//     next()
-//   }
-// })
-
-
-
+app.get('/user/:id1', function (req, res) {
+  console.log('gettwo')
+  res.end()
+})
 app.get('/user/:id1', function (req, res) {
   console.log('gettwo')
   res.end()
