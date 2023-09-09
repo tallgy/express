d0134ef3c4c9b5d45840aeb8951b2f03dd1c846f
tallgy <1776232657@qq.com>
2023-09-09 22:47:48 +0800

    主要方法的执行逻辑

--- test/test.js
+++ test/test.js
@@ -2,21 +2,25 @@ let express = require('..')
 let app = express()
 
 app.use('/user/:id1', (req, res, next) => {
-  console.log(3);
+  // console.log(3);
   next();
 })
 
+app.use('/user/:id1', (req, res, next) => {
+  // console.log(4);
+  res.end('11');
+})
 app.get('/user1/:id', (req, res, next) => {
-  console.log(4);
+  // console.log(5);
   next()
 })
 
 app.get('/user/:id1', function (req, res) {
-  console.log('gettwo')
+  // console.log('6')
   res.end()
 })
 app.get('/user/:id1', function (req, res) {
-  console.log('gettwo')
+  // console.log('6')
   res.end()
 })
 
