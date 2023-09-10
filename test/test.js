809029cacf67cb0e8a693eb06b7d581108c51ea4
tallgy <1776232657@qq.com>
2023-09-10 22:48:39 +0800

    test 文件

--- test/test.js
+++ test/test.js
@@ -4,15 +4,27 @@ let app = express()
 app.use('/user/:id1', (req, res, next) => {
   // console.log(3);
   next();
+  // throw('错误')
 })
 
 app.get('/user/:id1', function (req, res, next) {
   // console.log('6')
   // res.end()
-  next();
-}, function (req, res) {
+  next('route');
+}, function (err, req, res, next) {
+  console.log('111111111');
   res.end('11')
 })
+
+app.get('/user/:id1', function (err, req, res, next) {
+  console.log('111111111');
+  res.end('11')
+})
+
+app.use('/user/:id1', (err, req, res, next) => {
+  next('存在错误');
+})
+
 
 app.get('/user1/q', function (req, res) {
   // console.log('6')
