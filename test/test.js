be76610f9a2fd6ca2c8d470262db4c46e9bf7408
tallgy <1776232657@qq.com>
2023-09-11 23:08:50 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,35 +1,36 @@
-let express = require('..')
+let express = require('express')
 let app = express()
 
 app.use('/user/:id1', (req, res, next) => {
-  // console.log(3);
+  console.log(1);
   next();
-  // throw('错误')
+  console.log(2);
 })
 
 app.get('/user/:id1', function (req, res, next) {
-  // console.log('6')
-  // res.end()
-  next('route');
+  console.log(3);
+  next();
+  console.log(4);
 }, function (err, req, res, next) {
   console.log('111111111');
   res.end('11')
 })
 
-app.get('/user/:id1', function (err, req, res, next) {
-  console.log('111111111');
+app.get('/user/:id1', function (req, res, next) {
+  console.log(5);
   res.end('11')
+  console.log(6);
 })
 
-app.use('/user/:id1', (err, req, res, next) => {
-  next('存在错误');
-})
+// app.use('/user/:id1', (err, req, res, next) => {
+//   next('存在错误');
+// })
 
 
-app.get('/user1/q', function (req, res) {
-  // console.log('6')
-  res.end('qqq')
-})
+// app.get('/user1/q', function (req, res) {
+//   // console.log('6')
+//   res.end('qqq')
+// })
 app.listen(3000,() => console.log('Server is running...'))
 
 /*
